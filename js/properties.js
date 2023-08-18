47208551f9d64fcefbc31b22b0663316bcd70c12
amitoufo <137852286+amitoufo@users.noreply.github.com>
2023-08-18 16:22:52 +0800

    updateeee

--- js/properties.js
+++ js/properties.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://8ao0o5ibtl.execute-api.ap-southeast-2.amazonaws.com/test/kiranproperties';
+const apiUrl = 'https://3qprmpuv97.execute-api.ap-southeast-2.amazonaws.com/dev/kiran-property';
 let allProperties = []
 
 // fetch the data
@@ -35,7 +35,7 @@ function displayData(properties) {
             const deleteButton = document.createElement('button');
             deleteButton.textContent = 'Delete';
             deleteButton.addEventListener('click', () => {
-                deleteProperty(properties.id); // Make sure properties.id is the correct property ID
+                deleteProperty(properties.property_id); // Make sure properties.property_id is the correct property ID
             });
             propertiesBox.appendChild(deleteButton);
 
@@ -75,18 +75,20 @@ function displayData(properties) {
             propertiesAmenities.classList.add('properties-amenities');
             propertiesAmenities.textContent = "Amenities: " + properties.amenities;
 
-            propertiesBox.appendChild(propertiesName);
-            propertiesBox.appendChild(propertiesPrice);
-            propertiesBox.appendChild(propertiesDescription);
-            propertiesBox.appendChild(propertiesLocation);
-            propertiesBox.appendChild(propertiesType);
-            propertiesBox.appendChild(propertiesSize);
-            propertiesBox.appendChild(propertiesBathroom);
-            propertiesBox.appendChild(propertiesBedroom);
-            propertiesBox.appendChild(propertiesAmenities);
+            propertiesBox.appendChild(propertiesName); // 1st item
+            propertiesBox.appendChild(propertiesPrice); // 2nd item
+            propertiesBox.appendChild(propertiesDescription); // 3rd item
+            propertiesBox.appendChild(propertiesLocation); // 4th item
+            propertiesBox.appendChild(propertiesType); // 5th item
+            propertiesBox.appendChild(propertiesSize); // 6th item
+            propertiesBox.appendChild(propertiesBathroom); // 7th item
+            propertiesBox.appendChild(propertiesBedroom); // 8th item
+            propertiesBox.appendChild(propertiesAmenities); // 9th item
+
+            propertiesBox.appendChild(editButton); // This will be outside the 3x3 grid
+            propertiesBox.appendChild(deleteButton); // This will be outside the 3x3 grid
 
             propertiesDiv.appendChild(propertiesBox);
-
             propertiesBox.style.marginBottom = '20px';
         });
     } else {
@@ -118,7 +120,7 @@ function openEditModal(property) {
     ];
 
     for (const propertyKey of propertyKeys) {
-        if (propertyKey !== 'id') { // Exclude the 'id' property
+        if (propertyKey !== 'property_id') { // Exclude the 'property_id' property
             const label = document.createElement('label');
             label.textContent = propertyKey.replace(/_/g, ' ');
 
@@ -140,27 +142,32 @@ function openEditModal(property) {
         inputElements.forEach(input => {
             formData[input.previousSibling.textContent.replace(/ /g, '_')] = input.value;
         });
-    
+
+        const apiUrl = 'https://3qprmpuv97.execute-api.ap-southeast-2.amazonaws.com/dev/kiran-property'; // Replace with your API endpoint URL
+
         // Send PUT request to update property
-        fetch(apiUrl + '/' + property.id, {
+        fetch(apiUrl + '/' + property.property_id, {
             method: 'PUT',
             headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(formData)
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error updating property');
+            }
+            return response.json();
+        })
         .then(updatedPropertyData => {
             // Update the property directly in the allProperties array
-            const updatedIndex = allProperties.findIndex(prop => prop.id === updatedPropertyData.id);
+            const updatedIndex = allProperties.findIndex(prop => prop.property_id === updatedPropertyData.property_id);
             if (updatedIndex !== -1) {
                 allProperties[updatedIndex] = updatedPropertyData;
-    
+
                 // Update the displayed properties
                 displayData(allProperties);
-    
+
                 modal.remove();
             }
         })
@@ -175,18 +182,20 @@ function openEditModal(property) {
     document.body.appendChild(modal);
 }
 
+
 function addProperty() {
 
     const newProperty = {
-        name: "New Property",
-        rental_price: 0,
+        property_id: "Enter Property ID",
+        amenities: "Amenities of the new property",
         description: "Description of the new property",
         location: "Location of the new property",
-        property_type: "Property Type",
-        size: "Size of the new property",
         no_of_bathroom: 0,
         no_of_bedroom: 0,
-        amenities: "Amenities of the new property"
+        property_type: "Property Type",
+        rental_price: 0,
+        size: "Size of the new property",
+        name: "New Property"
     };
 
     const modal = document.createElement('div');
@@ -212,6 +221,7 @@ function addProperty() {
         const input = document.createElement('input');
         input.setAttribute('type', 'text');
         input.setAttribute('placeholder', newProperty[propertyKey]);
+        input.value = newProperty[propertyKey]; // Set the default value
 
         form.appendChild(label);
         form.appendChild(input);
@@ -226,62 +236,12 @@ function addProperty() {
         let allFieldsFilled = true;
 
         const inputElements = form.querySelectorAll('input');
-        inputElements.forEach(input => {
-            formData[input.previousSibling.textContent.replace(/\s+/g, '_')] = input.value;
-        });
-
-        // Send POST request to add new property
-        fetch(apiUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': true
-            },
-            body: JSON.stringify(formData)
-        })
-        .then(response => response.json())
-        .then(newPropertyData => {
-            // Add the new property to the displayed properties
-            allProperties.push(newPropertyData);
-            displayData(allProperties);
-
-            modal.remove();
-        })
-        .catch(error => {
-            console.error('Error adding property:', error);
-            modal.remove();
-        });
-    });
-
-    form.appendChild(submitButton);
-    modal.appendChild(form);
-    document.body.appendChild(modal);
-
-    form.appendChild(submitButton);
-    modal.appendChild(form);
-    document.body.appendChild(modal);
-    const errorMessage = document.createElement('div');
-    errorMessage.classList.add('error-message');
-    errorMessage.textContent = 'Please fill in all fields';
-    errorMessage.style.color = 'red';
-    errorMessage.style.display = 'none'; // Initially hide the error message
-
-form.appendChild(errorMessage);
-
-submitButton.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the form from being submitted
-    
-    const formData = {};
-    let allFieldsFilled = true;
-
-    const inputElements = form.querySelectorAll('input');
     inputElements.forEach(input => {
         const propertyName = input.previousSibling.textContent.replace(/\s+/g, '_');
         const value = input.value.trim();
         
         if (value === '') {
-            allFieldsFilled = false;
+            allFieldsFilled = false; // If any field is empty, set to false
             input.classList.add('invalid');
         } else {
             input.classList.remove('invalid');
@@ -298,19 +258,90 @@ submitButton.addEventListener('click', (event) => {
         errorMessage.style.display = 'none'; // Hide the error message
         modal.classList.remove('modal-open'); // Close the modal
     }
+        console.log('formData:', formData);
+
+        // Send POST request to add new property
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error adding property');
+            }
+            return response.json();
+        })
+        .then(newPropertyData => {
+            // Add the new property to the displayed properties
+            allProperties.push(newPropertyData);
+            displayData(allProperties);
+
+            modal.remove();
+        })
+        .catch(error => {
+            console.error('Error adding property:', error);
+            modal.remove();
+        });
+    });
+
+    form.appendChild(submitButton);
+    modal.appendChild(form);
+    document.body.appendChild(modal);
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('error-message');
+    errorMessage.textContent = 'Please fill in all fields';
+    errorMessage.style.color = 'red';
+    errorMessage.style.display = 'none'; // Initially hide the error message
+
+    form.appendChild(errorMessage);
+
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the form from being submitted
+    
+        const formData = {};
+        let allFieldsFilled = true;
+
+        const inputElements = form.querySelectorAll('input');
+        inputElements.forEach(input => {
+            const propertyName = input.previousSibling.textContent.replace(/\s+/g, '_');
+            const value = input.value.trim();
+        
+            if (value === '') {
+                allFieldsFilled = false;
+                input.classList.add('invalid');
+            } else {
+                input.classList.remove('invalid');
+            }
+
+            formData[propertyName] = value;
+        });
+
+        if (!allFieldsFilled) {
+            errorMessage.style.display = 'block'; // Show the error message
+            modal.classList.add('modal-open'); // Keep the modal open
+            return; // Stop further processing
+        } else {
+            errorMessage.style.display = 'none'; // Hide the error message
+            modal.classList.remove('modal-open'); // Close the modal
+        }
     
         if (allFieldsFilled) {
-            // Send POST request to add new property
             fetch(apiUrl, {
                 method: 'POST',
                 headers: {
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': true
+                    'Content-Type': 'application/json'
                 },
                 body: JSON.stringify(formData)
             })
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('An error occurred');
+                }
+                return response.json();
+            })
             .then(newPropertyData => {
                 // Add the new property to the displayed properties
                 allProperties.push(newPropertyData);
@@ -327,23 +358,27 @@ submitButton.addEventListener('click', (event) => {
 
 }
 
-function deleteProperty(propertyId) {
+
+function deleteProperty(property_id) {
     const confirmation = confirm("Are you sure you want to delete this property?");
     if (!confirmation) {
         return; // Abort the deletion
     }
 
     // Send DELETE request to remove property
-    fetch(apiUrl + '/' + propertyId, {
+    fetch(apiUrl + '/' + property_id, {
         method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
     .then(response => {
         if (response.status === 200) {
-            // Remove the property from the allProperties array
-            allProperties = allProperties.filter(prop => prop.id !== propertyId);
+             // Remove the property from the allProperties array
+             allProperties = allProperties.filter(prop => prop.property_id !== property_id);
 
-            // Update the displayed properties
-            displayData(allProperties);
+             // Update the displayed properties
+             displayData(allProperties);
         }
     })
     .catch(error => {
